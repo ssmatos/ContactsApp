790043f4917b8225474ea2c828e9b6a06857092a
ssmatos <sebastiansoto.fms@gmail.com>
2020-02-22 08:05:42 -0400

    Some changes

---
+++ ContactsApp/ContactsApp/ContactsApp/Models/Contact.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContactsApp.Models
+{
+	public class Contact
+	{
+		public string Name { get; set; }
+		public string Number { get; set; }
+		public string Image { get; set; }
+	}
+}
